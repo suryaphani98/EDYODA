83498471dc037221b33fa432376d7c5304e37fbb
Surya Phani <67063022+suryaphani98@users.noreply.github.com>
2023-03-04 11:11:51 +0530

    Add files via upload

---
+++ practise_0203.py
@@ -0,0 +1,38 @@
+s = "Surya"
+
+sub_seq = s[0:2]
+
+print(sub_seq)
+
+i = 1
+while i<=6:
+    print(i)
+    i=i+1
+
+
+num = 1
+while(num<10):
+    print(num,"......",num**2)
+    num=num+1
+
+
+a = ['apple','banan','orange']
+
+for i in a:
+    print(i)
+
+
+
+
+s4 = "surya"
+
+print(s4[::-1])
+
+
+x=0
+y=1
+
+while y<=50:
+    x,y = y,x+y
+    print(x)
+    
