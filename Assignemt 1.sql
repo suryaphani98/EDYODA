e01b6677349969ca0f289869a10a12a078ed7bd8
Surya Phani <67063022+suryaphani98@users.noreply.github.com>
2023-05-21 23:03:29 +0530

    Update Assignemt 1.sql

--- Assignemt 1.sql
+++ Assignemt 1.sql
@@ -1,4 +1,4 @@
-/* 1. Count the number of Salesperson whose name begin with ‘a’/’A’*/                                  
+/* 1. Count the number of Salesperson whose name begin with â€˜aâ€™/â€™Aâ€™*/                                  
 
 select * from SalesPeople where Sname like 'A%' or 'a%';
 
@@ -9,15 +9,15 @@ where Amt > 2000;
 
 /* 3. Count the number of Salesperson belonging to Newyork.*/
 
-select count(*) from SalesPeople where city = 'Newyork';
+select count(*) from SalesPeople where City = 'Newyork';
 
 /* 4. Display the number of Salespeople belonging to London and belonging to Paris.*/ 
 
-select * from salespeople where city in ('London','Paris');
+select count(*) from salespeople where City in ('London','Paris');
 
 /*5. Display the number of orders taken by each Salesperson and their date of orders.*/
 
-select s.Sname,count(*) as orders_count from orders o join salespeople s on s.Snum = o.Snum
+select s.Sname,count(*) as orders_count from orders o join salespeople s on o.Odate = o.Snum
 group by o.Snum;
 
 
